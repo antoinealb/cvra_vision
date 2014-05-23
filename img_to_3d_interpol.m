47bff77a5199b668ef34c99670aefa0a7dcc8907
Phil-Dominik Reukauf <pd.reukauf@gmail.com>
2014-05-23 15:44:29 +0200

    interpolation to find 2nd degree polynomial function from img coordinates to 3D coordinates

---
+++ img_to_3d_interpol.m
@@ -0,0 +1,12 @@
+% interpolation of image coordinates to 3D using img01.jpg
+
+x_pxl = [24, 48, 72, 96, 122, 147, 174, 200, 228, 254, ...
+    284, 314, 344, 368, 400, 429, 466, 500, 536, 572, ...
+    609, 646, 687, 724, 771, 813, 855, 902, 951];           % [pxl]
+y_mm = [10:10:290];                                         % [mm] distance from top of image
+
+P = polyfit(x_pxl, y_mm, 2)
+f = @(x) P(3) + P(2).*x + P(1).*x.^2;
+
+figure;
+plot(x_pxl, y_mm, 'b', x_pxl, f(x_pxl), 'r');
